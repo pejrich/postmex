c04e86999b24134a9b59fcc5e036b91262dd650a
Peter R <pejrich@gmail.com>
2019-04-16 12:36:32 -0700

    fixed client

--- lib/postmex/client.ex
+++ lib/postmex/client.ex
@@ -3,7 +3,7 @@ defmodule Postmex.Client do
 
   def get_delivery_quote(pickup_address, dropoff_address, client_id \\ nil) do
     url = Postmex.Url.delivery_quote_url(client_id)
-    body = {:form, [dropoff_address: dropoff_address_val, pickup_address: pickup_address_val]}
+    body = {:form, [dropoff_address: dropoff_address, pickup_address: pickup_address]}
     headers = Postmex.auth_header()
     post!(url, body, headers)
   end
