c2d2889f7db56c28d0775074c9b757b3b6f895f4
Peter R <pejrich@gmail.com>
2019-04-22 16:05:44 -0700

    add plug

--- mix.exs
+++ mix.exs
@@ -21,6 +21,7 @@ defmodule Postmex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:plug, "~> 1.7"},
       {:httpoison, "~> 1.4"},
       {:vex, "~> 0.8.0"}
     ]
